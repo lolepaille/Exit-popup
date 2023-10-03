3675cc037eabe646ed898c1382f9ef08c7bfdc3f
Lawrence Makoona <lawrence@lide-labs.com>
2023-10-03 16:28:44 +1100

    Author

--- exit-intent-popup/exit-intent-popup.php
+++ exit-intent-popup/exit-intent-popup.php
@@ -3,7 +3,7 @@
 Plugin Name: Exit Intent Popup
 Description: Display a popup when the user leaves the page.
 Version: 1.0
-Author: Your Name
+Author: Lawrence
 */
 
 // Enqueue the JavaScript and CSS files
